f938d1dd167528b40ce0efc9c440eebba41b153e
Dalun Chen <dalunchen.fi@gmail.com>
2017-10-19 10:43:11 +0300

    replaced the API data object with Postgre data object. Set digits = 0 in renderTable()

--- app.R
+++ app.R
@@ -1,7 +1,6 @@
 library(shiny)
 library(jsonlite)
 library(RPostgreSQL)
-# library(curl)
 
 # connect to a local postgreDB
 pw <- { 
@@ -25,13 +24,12 @@ df_parkingstations_details <- data.frame()
 df_parkingstations_details$name <- character()
 df_parkingstations_details$timestamp <- character()
 df_parkingstations_details$address <- character()
-# df_parkingstations_details$timestamp <- timestamp()
 df_parkingstations_details$freespaces <- integer()
 df_parkingstations_details$totalspaces <- integer()
 
 # fetch parking station details from an API
 for(i in 1:nrow(df_parkingstations)){
-  api_url <- paste0(paste0("https://www.oulunliikenne.fi/public_traffic_api/parking/parking_details.php?parkingid=", as.character(df_parkingstations[i, 1]), collapse = NULL))
+  api_url <- paste0("https://www.oulunliikenne.fi/public_traffic_api/parking/parking_details.php?parkingid=", as.character(df_parkingstations[i, 1]), collapse = NULL)
   parkingstation_details <- fromJSON(api_url)
   df_parkingstations_details[i, 1] <- parkingstation_details$name
   df_parkingstations_details[i, 2] <- parkingstation_details$timestamp
@@ -45,9 +43,6 @@ for(i in 1:nrow(df_parkingstations)){
   }
 }
 
-# fetch the last row in the freespaces_update table
-last_row_freespaces_update <- dbGetQuery(connection, "SELECT timestamp FROM freespaces_update WHERE name = 'Ouluhalli' ORDER BY timestamp DESC LIMIT 1")
-
 # write data to the table 
 dbWriteTable(connection, "freespaces_update", 
              value = df_parkingstations_details, 
@@ -56,53 +51,6 @@ dbWriteTable(connection, "freespaces_update",
 # close db connection
 dbDisconnect(connection)
 dbUnloadDriver(postgreDriver)
-
-parkingPlaces <- fromJSON("https://www.oulunliikenne.fi/public_traffic_api/parking/parkingstations.php")
-parkingPlaces$parkingstation$address <- "address"
-parkingPlaces$parkingstation$timestamp <- "timestamp"
-parkingPlaces$parkingstation$freespace <- "0"
-parkingPlaces$parkingstation$totalspace <- "0"
-#timeFetched <- "time"
-#totalParkingPlaces <- nrow(parkingPlaces$parkingstation)
-for(i in 1:length(parkingPlaces$parkingstation$id)){
-  detailUrl <- paste0("https://www.oulunliikenne.fi/public_traffic_api/parking/parking_details.php?parkingid=", as.character(parkingPlaces$parkingstation[i, 1]), collapse = NULL)
-  parkingPlace <- fromJSON(detailUrl)
-  # if(i == 1){
-  #   timeFetched <<- parkingPlace$timestamp
-  # }
-  if("freespace" %in% names(parkingPlace)){
-    parkingPlaces$parkingstation[i,4] <- parkingPlace$address
-    parkingPlaces$parkingstation[i,5] <- parkingPlace$timestamp
-    parkingPlaces$parkingstation[i,6] <- parkingPlace$freespace
-    parkingPlaces$parkingstation[i,7] <- parkingPlace$totalspace
-  } else {
-    parkingPlaces$parkingstation[i,4] <- parkingPlace$address
-    parkingPlaces$parkingstation[i,5] <- parkingPlace$timestamp
-    #next
-    #parkingPlaces$parkingstation[i,4] <- "0"
-  }
-}
-
-# # Function to fetch free spaces
-# fetchFreeSpaces <- function(){
-#   for(i in 1:length(parkingPlaces$parkingstation$id)){
-#     detailUrl <- paste0("https://www.oulunliikenne.fi/public_traffic_api/parking/parking_details.php?parkingid=", as.character(parkingPlaces$parkingstation[i, 1]), collapse = NULL)
-#     parkingPlace <- fromJSON(detailUrl)
-#     # if(i == 1){
-#     #   timeFetched <<- parkingPlace$timestamp
-#     # }
-#     if("freespace" %in% names(parkingPlace)){
-#       #parkingPlaces$parkingstation[i,4] <- parkingPlace$address
-#       parkingPlaces$parkingstation[i,5] <- parkingPlace$timestamp
-#       parkingPlaces$parkingstation[i,6] <<- parkingPlace$freespace
-#       #parkingPlaces$parkingstation[i,6] <- parkingPlace$totalspace
-#     } else {
-#       #parkingPlaces$parkingstation[i,4] <- parkingPlace$address
-#       next
-#       #parkingPlaces$parkingstation[i,4] <- "0"
-#     }
-#   }
-# }
 
 # UI
 ui <- fluidPage(
@@ -121,25 +69,18 @@ ui <- fluidPage(
   fluidRow(
     column(
       3,
-      #plotOutput("barChart")
       h2("Select a parking place to see the details:"),
       selectInput("selectedParkingPlace", NULL,
-                  choices = parkingPlaces$parkingstation$name,
+                  choices = df_parkingstations$name,
                   selected = NULL),
       htmlOutput("result")
-      #textOutput("result")
     ),
     column(
       9,
       plotOutput("barChart"),
-      h2("Combined data from a local postgreDB and an Oulu city API"),
-      # h2("Original Data from Oulu city:"),
-      # textOutput("timeFetched"),
+      h2("Data fetched from Oulu city API:"),
       tableOutput("table"),
-      p("station_id, geom and name are from the local DB, whereas timestamp, address, freespaces and total spaces are from the API below:"),
-      # p("Data is fetched from the APIs below:"),
-      # tags$a(href="https://www.oulunliikenne.fi/public_traffic_api/parking/parkingstations.php", "www.oulunliikenne.fi/public_traffic_api/parking/parkingstations.php"),
-      # tags$br(),
+      p("Above data is from the API below (the id of the station is used in query parameter):"),
       tags$a(href="https://www.oulunliikenne.fi/public_traffic_api/parking/parking_details.php?parkingid=2", "www.oulunliikenne.fi/public_traffic_api/parking/parking_details.php?parkingid=2")
     )
   )
@@ -147,19 +88,14 @@ ui <- fluidPage(
 
 # Define required server logic
 server <- function(input, output) {
-  
   output$barChart <- renderPlot({
-    freespaces <- as.numeric(parkingPlaces$parkingstation$freespace)
-    barplot(freespaces, xlab ="parking places", ylab="free spaces", names.arg = parkingPlaces$parkingstation$name, col = "orange")
+    freespaces <- as.numeric(df_parkingstations_details$freespaces)
+    barplot(freespaces, xlab ="parking places", ylab="free spaces", names.arg = df_parkingstations_details$name, col = "orange")
   })
-  # output$timeFetched <- renderText(timeFetched)
-  output$table <- renderTable(df_parkingstations_details)
-  # output$table <- renderTable(df_parkingstations)
-  # output$table <- renderTable(parkingPlaces$parkingstation)
+  output$table <- renderTable(df_parkingstations_details, digits = 0)
   output$result <- renderText({
-    parkingPlaceDetails <- parkingPlaces$parkingstation[parkingPlaces$parkingstation$name == input$selectedParkingPlace,]
-
-    paste("On", parkingPlaceDetails$timestamp,"<br>", parkingPlaceDetails$name, "has" , parkingPlaceDetails$freespace, "free spaces")
+    parkingPlaceDetails <- df_parkingstations_details[df_parkingstations_details$name == input$selectedParkingPlace,]
+    paste("On", parkingPlaceDetails$timestamp,"<br>", parkingPlaceDetails$name, "has" , parkingPlaceDetails$freespaces, "free spaces")
     })
 }
 
